01279c42f0978afaca877da41c5edb5f38e76272
Miguel Prada <mapra99@gmail.com>
2020-07-05 18:59:10 -0500

    reactored validation of published field

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Post, type: :model do
     it 'validates presence of required fields' do
       should validate_presence_of(:title)
       should validate_presence_of(:content)
-      should validate_presence_of(:published)
+      should validate_inclusion_of(:published).in_array([true, false])
       should validate_presence_of(:user_id)
     end
   end
