c9c511b7133fe51a76150efaafac106492e35847
Miguel Prada <mapra99@gmail.com>
2020-07-18 20:45:55 -0500

    bg job

---
+++ app/jobs/post_report_job.rb
@@ -0,0 +1,9 @@
+class PostReportJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id, post_id)
+    user = User.find(user_id)
+    post = Post.find(post_id)
+    report = PostReport.generate(post)
+  end
+end
