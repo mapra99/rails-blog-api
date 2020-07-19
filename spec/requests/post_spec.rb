4b8070fdacfed373dbc5c1d66321f54150300044
Miguel Prada <mapra99@gmail.com>
2020-07-18 20:08:02 -0500

    refactored old post tests

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -51,7 +51,7 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET /posts/{id}' do
-    let!(:post) { create(:post) }
+    let!(:post) { create(:post, published: true) }
 
     it 'should return content of one post' do
       get "/posts/#{post.id}"
@@ -71,83 +71,6 @@ RSpec.describe 'Posts', type: :request do
       get "/posts/0"
       payload = JSON.parse(response.body)
       expect(response).to have_http_status(404)
-    end
-  end
-
-  describe 'POST /posts' do
-    let!(:user) { create(:user) }
-
-    it 'should create a post' do
-      req_payload = {
-        post: {
-          title: Faker::Lorem.sentence,
-          content: Faker::Lorem.paragraph,
-          published: false,
-          user_id: user.id
-        }
-      }
-
-      post "/posts", params: req_payload
-      resp_payload = JSON.parse(response.body)
-      expect(resp_payload).not_to be_empty
-      expect(resp_payload['id']).not_to be_nil
-      expect(response).to have_http_status(:created)
-    end
-
-    it 'should return an error on invalid post' do
-      req_payload = {
-        post: {
-          content: Faker::Lorem.paragraph,
-          published: false,
-          user_id: user.id
-        }
-      }
-
-      post "/posts", params: req_payload
-      resp_payload = JSON.parse(response.body)
-      expect(resp_payload).not_to be_empty
-      expect(resp_payload['error']).not_to be_empty
-      expect(response).to have_http_status(:unprocessable_entity)
-    end
-  end
-
-  describe 'PUT /posts/{id}' do
-    let!(:article) { create(:post) }
-
-    it 'should edit a post' do
-      new_post_attributes = {
-        title: Faker::Lorem.sentence,
-        content: Faker::Lorem.paragraph,
-        published: false
-      }
-
-      req_payload = {
-        post: new_post_attributes
-      }
-
-      put "/posts/#{article.id}", params: req_payload
-      resp_payload = JSON.parse(response.body)
-      expect(resp_payload).not_to be_empty
-      expect(resp_payload['id']).to eq(article.id)
-      expect(response).to have_http_status(200)
-    end
-
-    it 'should return an error on invalid post' do
-      new_post_attributes = {
-        title: nil,
-        content: nil,
-        published: false
-      }
-
-      req_payload = {
-        post: new_post_attributes
-      }
-
-      put "/posts/#{article.id}", params: req_payload
-      resp_payload = JSON.parse(response.body)
-      expect(resp_payload).not_to be_empty
-      expect(resp_payload['error']).not_to be_empty
-      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
