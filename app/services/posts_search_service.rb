fdf6a0cafdd29853cf88daf76dd8d470b829fd24
Miguel Prada <mapra99@gmail.com>
2020-07-18 20:25:51 -0500

    cached posts queries

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,9 @@
 class PostsSearchService
-  def self.search current_posts, query
-    current_posts.where("title like '%#{query}%'")
+  def self.search(current_posts, query)
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hour) do
+      current_posts.where("title like '%#{query}%'").pluck(:id)
+    end
+
+    current_posts.where(id: posts_ids)
   end
 end
