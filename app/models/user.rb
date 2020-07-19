9fd4de7ab4fd90dee6794397b46b364ccab26aaf
Miguel Prada <mapra99@gmail.com>
2020-07-18 19:18:59 -0500

    token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :name, presence: true
   validates :auth_token, presence: true
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    return if auth_token.present?
+
+    self.auth_token = TokenGenerationService.generate
+  end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     name { Faker::Name.name }
-    auth_token { "xxxxxxx" }
+    auth_token { TokenGenerationService.generate }
   end
 end
